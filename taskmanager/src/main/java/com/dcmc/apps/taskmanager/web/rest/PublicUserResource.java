9898432eedfc7c2354886673039c541a04c1ad90
ulloa <culloa@clicklease.com>
2025-06-10 11:23:02 -0600

    Add example using SecurityUtils and ROLES

--- taskmanager/src/main/java/com/dcmc/apps/taskmanager/web/rest/PublicUserResource.java
+++ taskmanager/src/main/java/com/dcmc/apps/taskmanager/web/rest/PublicUserResource.java
@@ -1,8 +1,7 @@
 package com.dcmc.apps.taskmanager.web.rest;
 
-import com.dcmc.apps.taskmanager.service.UserService;
-import com.dcmc.apps.taskmanager.service.dto.UserDTO;
-import java.util.*;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -10,8 +9,17 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.dcmc.apps.taskmanager.security.AuthoritiesConstants;
+import com.dcmc.apps.taskmanager.security.SecurityUtils;
+import com.dcmc.apps.taskmanager.service.UserService;
+import com.dcmc.apps.taskmanager.service.dto.UserDTO;
+
 import tech.jhipster.web.util.PaginationUtil;
 
 @RestController
@@ -35,6 +43,24 @@ public class PublicUserResource {
     @GetMapping("/users")
     public ResponseEntity<List<UserDTO>> getAllPublicUsers(@org.springdoc.core.annotations.ParameterObject Pageable pageable) {
         LOG.debug("REST request to get all public User names");
+        
+        System.out.println(SecurityUtils.getCurrentUserLogin().get());
+        System.out.println(SecurityUtils.isAuthenticated());
+        System.out.println(SecurityUtils.hasCurrentUserThisAuthority(AuthoritiesConstants.ADMIN));
+
+        final Page<UserDTO> page = userService.getAllPublicUsers(pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
+    }
+    
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @GetMapping("/users2")
+    public ResponseEntity<List<UserDTO>> getAllPublicUsers2(@org.springdoc.core.annotations.ParameterObject Pageable pageable) {
+        LOG.debug("REST request to get all public User names");
+        
+        System.out.println(SecurityUtils.getCurrentUserLogin().get());
+        System.out.println(SecurityUtils.isAuthenticated());
+        System.out.println(SecurityUtils.hasCurrentUserThisAuthority(AuthoritiesConstants.ADMIN));
 
         final Page<UserDTO> page = userService.getAllPublicUsers(pageable);
         HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
